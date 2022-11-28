af04f3088e761a4791377fd6b80548093b7a23ab
DharaniSadasivam <43529198+DharaniSadasivam@users.noreply.github.com>
2022-11-28 07:59:01 +0530

    Initial project setup

---
+++ MXPagerViewReloadUpdate/MXPagerViewReloadUpdate.h
@@ -0,0 +1,18 @@
+//
+//  MXPagerViewReloadUpdate.h
+//  MXPagerViewReloadUpdate
+//
+//  Created by Dharani Sadasivam on 28/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MXPagerViewReloadUpdate.
+FOUNDATION_EXPORT double MXPagerViewReloadUpdateVersionNumber;
+
+//! Project version string for MXPagerViewReloadUpdate.
+FOUNDATION_EXPORT const unsigned char MXPagerViewReloadUpdateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MXPagerViewReloadUpdate/PublicHeader.h>
+
+
---
+++ MXPagerViewReloadUpdateTests/MXPagerViewReloadUpdateTests.m
@@ -0,0 +1,36 @@
+//
+//  MXPagerViewReloadUpdateTests.m
+//  MXPagerViewReloadUpdateTests
+//
+//  Created by Dharani Sadasivam on 28/11/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MXPagerViewReloadUpdateTests : XCTestCase
+
+@end
+
+@implementation MXPagerViewReloadUpdateTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
